a014ed8dd6f53cfacdee6c15e7e99e123143b72e
pedrocabral <pedrocabral@users.noreply.github.com>
2015-01-12 15:45:50 -0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package brick
+ */
+?>
+		</div><!-- #content --><!-- footer.php -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<p>Footer<span class="sep"> | </span>brick by <a href="http://github.com/vodooweb/" rel="designer">vodooweb</a></p>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+<?php wp_footer(); ?>
+</body>
+</html><!-- footer.php -->

975c38c31e420747e50f43e219fbc6b94d16ffdb
pedrocabral <pedrocabral@users.noreply.github.com>
2015-01-12 19:06:04 -0200

    Update content-single.php

--- content-single.php
+++ content-single.php
@@ -6,6 +6,7 @@
 	<article id="post-<?php the_ID(); ?>" class="article-<?php the_slug();?>"><!-- content-single.php -->
 						<header class="entry-header">
 							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+							
 						</header><!-- .entry-header -->
 						<div class="entry-content">
 							<?php the_content();?>

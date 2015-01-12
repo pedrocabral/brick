cecbfdc75f939b668ec95106a800c1b4b06742ed
pedrocabral <pedrocabral@users.noreply.github.com>
2015-01-12 19:39:30 -0200

    Create page.php

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package brick
+ */
+
+get_header(); ?>
+
+			<div id="primary" class="content-area"><!-- page.php -->
+				<main id="main" class="site-main" role="main">
+					<?php while ( have_posts() ) : the_post();
+						get_template_part( 'content', 'page' );
+						endwhile; // end of the loop. ?>
+						<?php echo chr(0x0D).chr(0x0A);?>
+				</main><!-- #main -->
+			</div><!-- #primary --><!-- single.php -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

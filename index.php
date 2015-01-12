4c5c1c2da5baafae7986d2bbcdb78ff4627ad1eb
pedrocabral <pedrocabral@users.noreply.github.com>
2015-01-12 20:07:32 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package brick
+ */
+
+get_header(); ?>
+
+			<div id="primary" class="content-area"><!-- index.php -->
+				<main id="main" class="site-main" role="main">
+<?php if ( have_posts() ) :
+	while ( have_posts() ) : the_post();
+			get_template_part( 'content', get_post_format() );
+		endwhile;
+		else :
+			get_template_part( 'content', 'none' ); ?>
+<?php endif; ?>
+				</main><!-- #main -->
+			</div><!-- #primary --><!-- index.php -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

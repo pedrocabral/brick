66f790b36fc1a777e8c4b43b79f46e458f55ba93
pedrocabral <pedrocabral@users.noreply.github.com>
2015-01-16 19:58:49 -0200

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package brick
+ * @package akapulko
  */
 ?><!DOCTYPE html><!-- header.php -->
 <html <?php language_attributes(); ?>>
@@ -12,14 +12,17 @@
 <?php flush(); ?>
 <body <?php body_class(); ?>>
 	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'brick' ); ?></a>
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'akapulko' ); ?></a>
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</div><!-- .site-branding -->
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => null, 'items_wrap' => '<ul id="%1$s" class="%2$s">'."\n".'%3$s'."\t\t\t\t".'</ul>'."\n" ,'depth' => 5, 'walker' => new menu_walker() ) ); ?>
+				<?php
+				if ( has_nav_menu('primary') ){
+					wp_nav_menu( array( 'theme_location' => 'primary', 'container' => null, 'items_wrap' => '<ul id="%1$s" class="%2$s">'."\n".'%3$s'."\t\t\t\t".'</ul>'."\n" ,'depth' => 5, 'walker' => new menu_walker() ) );
+				}?>
 			</nav><!-- #site-navigation -->
 			<div class="search-form">
 				<?php get_search_form();
